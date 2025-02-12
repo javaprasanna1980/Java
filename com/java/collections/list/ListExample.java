2491445de28acc45da86bf935cab65c66babb88c
prasanna <javaprasanna1980@gmail.com>
2025-02-12 12:01:11 +0530

    1

---
+++ com/java/collections/list/ListExample.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class listExample{
+	public statis void main(String[] args){
+		
+		// Using ArrayList
+		List<String> arraylist = new ArrayList();
+		
+		// Using LinkedList
+		List<String> linkedlist = new ArrayList();
+		
+		// Adding elements
+		arraylist.add("Apple");
+		arraylist.add("Banana");
+	}
+}
